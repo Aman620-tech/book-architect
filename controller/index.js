2675875b83d2008bf277d9017aa5483c6e7e1984
BI-Aman pandey <109646821+BI-Aman-pandey@users.noreply.github.com>
2022-12-22 22:29:18 +0530

    New update

--- controller/index.js
+++ controller/index.js
@@ -1,9 +1,12 @@
+const { Op } = require("sequelize");
 const {
   sequelize,
   wholeSaler,
   retailer,
   stock,
 } = require("../database/connect");
+
+// const stock = require("../database/stock");
 // const stock = require("../database/stock");
 // const wholeSale = require("../database/wholeSale");
 
@@ -27,23 +30,59 @@ const allRetailer = async (req, res) => {
 
 const retailerSingleWholeSaler = async (req, res) => {
   try {
- 
-
-    res.json({ status: 200, response: "yearly turnover of user " });
+    const Stock = await stock.findAll({
+      attributes: [
+        "retailerId",
+        [sequelize.fn("COUNT", sequelize.col("retailerId")), "count"],
+      ],
+      group: "retailerId",
+    });
+    const filterData = await Stock.filter((k) => {
+      return k.dataValues.count === 1;
+    });
+    res.json({
+      status: 200,
+      response: "single retailer ",
+      Stock,
+      result: filterData,
+    });
   } catch (err) {
     res.json({ status: 400, response: err.message });
   }
 };
 const monthlyTurnover = async (req, res) => {
   try {
-    const turnover = await stock.findAll({
-      attributes: [
-        "wholesalerId",
-      ],
+    let endDate = "2022-12-25";
+    // let newData = [];
+    let  turnover 
+    for (let i = 1; i <= 12; i++) {
+      let startDate = `2022-${i}-22`;
+      console.log("startDate", startDate);
+      turnover = await stock.findAll({
+        attributes: [
+              "wholesalerId",
+              "retailerId",
+              "createdAt",
+              [sequelize.fn("sum", sequelize.col("stock_amount")), "turnover"],
+            ],
+        where: {
+          createdAt: {
+            [Op.lt]: new Date(
+              new Date(endDate).getTime() + 60 * 60 * 24 * 1000 - 1
+            ),
+            [Op.gt]: new Date(startDate),
+          },
+        }, 
       group: ["wholesalerId"],
-    });
-    res.json({ status: 200, response: "Monthly turnover of wholesaler ", turnover });
 
+      });
+    }
+    res.json({
+      status: 200,
+      response: "Monthly turnover of wholesaler ",
+      turnover,
+      // newData,
+    });
   } catch (err) {
     res.json({ status: 400, response: err.message });
   }
